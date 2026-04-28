7c701c4570934a9b259e43259d0fce54369fddeb
Moshiul <gazimoshiul@gmail.com>
2026-04-29 00:33:38 +0600

    adding sorting functionality

--- src/components/TimelineContent.jsx
+++ src/components/TimelineContent.jsx
@@ -1,37 +1,61 @@
 "use client";
 import { CallButtonContext } from "@/context/callButton.context";
 import Image from "next/image";
-import { useContext } from "react";
+import { useContext, useEffect, useMemo, useState } from "react";
 import callIcon from "../assets/call.png";
 import textIcon from "../assets/text.png";
 import videoIcon from "../assets/video.png";
 import { format } from "date-fns";
+import { FaAngleDown } from "react-icons/fa";
 
 const TimelineContent = () => {
+  const [sortingType, setSortingType] = useState("");
   const { callFriends } = useContext(CallButtonContext);
+
+  // show current date as format
   const today = new Date();
   const currentDate = format(today, "MMMM dd, yyyy");
   console.log(callFriends, "Timeline");
+
+  // sorting calls based on type
+  const getFilteredCalls = () => {
+    if (!sortingType) return callFriends;
+    return callFriends.filter((call) => call.type === sortingType);
+  };
+
+  const filteredCalls = getFilteredCalls();
+
+  // reset all call
+  const showAllCalls = () => {
+    setSortingType("");
+  };
+
   return (
     <div className="">
       <div className="dropdown dropdown-right w-60">
-        <div tabIndex={0} role="button" className="btn m-1 w-58">
-          Filter timeline ➡️
+        <div tabIndex={0} role="button" className="btn mt-6 w-58">
+          Filter timeline <FaAngleDown />
         </div>
         <ul
           tabIndex="-1"
           className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
         >
-          <li>
-            <a>Item 1</a>
+          <li onClick={showAllCalls}>
+            <a>Filter timeline</a>
           </li>
-          <li>
-            <a>Item 2</a>
+          <li onClick={() => setSortingType("textToFriend")}>
+            <a>Text</a>
+          </li>
+          <li onClick={() => setSortingType("callToFriend")}>
+            <a>Call</a>
+          </li>
+          <li onClick={() => setSortingType("videoToFriend")}>
+            <a>Video</a>
           </li>
         </ul>
       </div>
       <div>
-        {callFriends.map((call, index) => (
+        {filteredCalls.map((call, index) => (
           <div
             key={index}
             className="flex items-center bg-white my-6 p-4 rounded-md shadow-md"
